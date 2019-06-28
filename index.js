93791e17c8b04072f32e718ab774496958ba329a
github username <github email address>
2019-06-28 14:57:18 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Write your solution in this file!
+
+const driver =  {}
+
+
+function  updateDriverWithKeyAndValue(driver,key,value){//does not mutate the original object
+return Object.assign({},driver,{[key]:value})
+
+}
+
+
+function destructivelyUpdateDriverWithKeyAndValue(driver,key,value){//DOES mutate the original object
+
+  driver[key]=value
+
+  return driver
+}
+
+
+function deleteFromDriverByKey(driver,key){//non-destructive
+
+  const newObj=Object.assign({},driver)
+
+  delete newObj[key]
+
+  return newObj
+
+}
+
+function destructivelyDeleteFromDriverByKey(driver,key){//does modifes the object
+
+  delete driver[key]
+
+  return driver
+
+
+}
